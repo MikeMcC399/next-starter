dacdb6b540564d2740cde123fd994ebe229900f4
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2026-08-10 10:09:46 +0200

    update to next 16 & cypress 15

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  allowCypressEnv: false,
+
   e2e: {
-    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
